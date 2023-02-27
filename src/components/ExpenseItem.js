9035da18e16ad9efdbf26df43ff8642327cafc9d
danishhai <dsandoval.ai@gmail.com>
2023-02-26 22:53:03 -0500

    adding some styles to Increase and Decrease Buttons

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -42,8 +42,10 @@ const ExpenseItem = (props) => {
         <tr>
         <td>{props.name}</td>
         <td>{currency}{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
-        <td><button onClick={event=> decreaseAllocation(props.name)}>-</button></td>
+        <td><button style={{backgroundColor: '#9ccc65', color:"white", borderRadius:"30%"}} 
+            onClick={event=> increaseAllocation(props.name)}>+</button></td>
+        <td><button style={{backgroundColor: '#ef5350', color:"white", borderRadius:"30%"}} 
+            class="decrease" onClick={event=> decreaseAllocation(props.name)}>-</button></td>
         <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
